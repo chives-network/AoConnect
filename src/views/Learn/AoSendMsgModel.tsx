79eeebda25b20e430364bd8f07153c4a5b5b54b4
ChatBookAI <reactchives@gmail.com>
2024-05-23 21:05:25 +0800

    SetAoConnectReminderProcessTxId

--- src/views/Learn/AoSendMsgModel.tsx
+++ src/views/Learn/AoSendMsgModel.tsx
@@ -127,7 +127,7 @@ const AoSendMsgModel = () => {
     }
   };
 
-  const [tags, setTags] = useState<string>('[ \n{ "name": "Your-Tag-Name-Here", "value": "your-tag-value" }, \n{ "name": "Another-Tag", "value": "another-value" } \n]')
+  const [tags, setTags] = useState<string>('[ \n{ "name": "Your-Tag-Name", "value": "Your-Tag-Value" }, \n{ "name": "Creator", "value": "Chives-Network" } \n]')
   const [tagsError, setTagsError] = useState<string | null>(null)
   const handleTagsChange = (event: any) => {
     setTags(event.target.value);
@@ -294,26 +294,7 @@ const AoSendMsgModel = () => {
                                 )
                             }}
                             error={!!processTxIdError}
-                            helperText={
-                                <Fragment>
-                                    {myProcessTxId && (
-                                    <>
-                                        <Typography variant='body2' sx={{}}>
-                                        {messageHelp}
-                                        </Typography>
-                                        <Link href={`https://cookbook_ao.g8way.io/concepts/processes.html`} target='_blank'>
-                                            {'Process Concept'}
-                                        </Link>
-                                        <Link href={`https://www.ao.link/processes`} target='_blank' sx={{ml: 4}}>
-                                            {'All Processes'}
-                                        </Link>
-                                        <Link href={`https://www.ao.link/entity/${processTxId}`} target='_blank' sx={{ml: 4}}>
-                                            {'Detail on AOLink'}
-                                        </Link>
-                                    </>
-                                    )}
-                                </Fragment>
-                                }
+                            helperText={messageHelp}
                         />
                         
                     </Grid>
