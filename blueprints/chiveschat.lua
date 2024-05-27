36ad25b72de339f39c03a3b7b99d5e34aa494dc7
ChatBookAI <reactchives@gmail.com>
2024-05-27 18:49:52 +0800

    return (Channels)

--- blueprints/chiveschat.lua
+++ blueprints/chiveschat.lua
@@ -31,12 +31,12 @@
 -- Send({Target = "CT8fSMyXjN_MQBGe1vFctW7gyGWneYGscP_jgjPi1yw", Action = "Broadcast", Data = "ChivesChat: Broadcasting My 1st Message" }) need user role to call
 -- Send({Target = "CT8fSMyXjN_MQBGe1vFctW7gyGWneYGscP_jgjPi1yw", Action = "Quit" }) need user role to call
 -- Setting Channels (need owner role to call)
--- Send({Target = "Dofexypz0gtPi5MmC7iHNw_d9E7nXVsHvO_BDriPfsk", Action = "AddChannel", ChannelId = "1", ChannelName = "Announcement", ChannelGroup = "Welcome", ChannelSort = "1", ChannelWritePermission = "Owner" })
--- Send({Target = "Dofexypz0gtPi5MmC7iHNw_d9E7nXVsHvO_BDriPfsk", Action = "AddChannel", ChannelId = "2", ChannelName = "Rules", ChannelGroup = "Welcome", ChannelSort = "2", ChannelWritePermission = "Owner" })
--- Send({Target = "Dofexypz0gtPi5MmC7iHNw_d9E7nXVsHvO_BDriPfsk", Action = "AddChannel", ChannelId = "3", ChannelName = "Introduction", ChannelGroup = "Introduction", ChannelSort = "3" })
--- Send({Target = "Dofexypz0gtPi5MmC7iHNw_d9E7nXVsHvO_BDriPfsk", Action = "AddChannel", ChannelId = "4", ChannelName = "Community", ChannelGroup = "Community", ChannelSort = "4" })
--- Send({Target = "Dofexypz0gtPi5MmC7iHNw_d9E7nXVsHvO_BDriPfsk", Action = "AddChannel", ChannelId = "5", ChannelName = "Support", ChannelGroup = "Community", ChannelSort = "5" })
--- Send({Target = "Dofexypz0gtPi5MmC7iHNw_d9E7nXVsHvO_BDriPfsk", Action = "AddChannel", ChannelId = "6", ChannelName = "Admin Team", ChannelGroup = "Administrators", ChannelSort = "6", ChannelReadPermission = "Owner,Admin" })
+-- Send({Target = "PoaM0gVaHif3PPdVbPd0ptWASXInF-UVEOd4cgFx5uM", Action = "AddChannel", ChannelId = "1", ChannelName = "Announcement", ChannelGroup = "Welcome", ChannelSort = "1", ChannelWritePermission = "Owner" })
+-- Send({Target = "PoaM0gVaHif3PPdVbPd0ptWASXInF-UVEOd4cgFx5uM", Action = "AddChannel", ChannelId = "2", ChannelName = "Rules", ChannelGroup = "Welcome", ChannelSort = "2", ChannelWritePermission = "Owner" })
+-- Send({Target = "PoaM0gVaHif3PPdVbPd0ptWASXInF-UVEOd4cgFx5uM", Action = "AddChannel", ChannelId = "3", ChannelName = "Introduction", ChannelGroup = "Introduction", ChannelSort = "3" })
+-- Send({Target = "PoaM0gVaHif3PPdVbPd0ptWASXInF-UVEOd4cgFx5uM", Action = "AddChannel", ChannelId = "4", ChannelName = "Community", ChannelGroup = "Community", ChannelSort = "4" })
+-- Send({Target = "PoaM0gVaHif3PPdVbPd0ptWASXInF-UVEOd4cgFx5uM", Action = "AddChannel", ChannelId = "5", ChannelName = "Support", ChannelGroup = "Community", ChannelSort = "5" })
+-- Send({Target = "PoaM0gVaHif3PPdVbPd0ptWASXInF-UVEOd4cgFx5uM", Action = "AddChannel", ChannelId = "6", ChannelName = "Admin Team", ChannelGroup = "Administrators", ChannelSort = "6", ChannelReadPermission = "Owner,Admin" })
 
 Owners = Owners or {}
 Admins = Admins or {}
@@ -60,7 +60,7 @@ Handlers.add(
   "GetInfo",
   Handlers.utils.hasMatchingTag("Action", "GetInfo"),
   function (msg)
-    return Channels
+    return (Channels)
   end
 )
 
